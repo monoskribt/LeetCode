1c4875eaa96373f9454143dba10913b33cab6cbb
monoskribt <kozub.dima3@gmail.com>
2025-01-09 20:35:02 +0100

    counting words with a given prefix

---
+++ src/Easy/CountingWordsWithAGivenPrefix.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class CountingWordsWithAGivenPrefix {
+
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+
+        for(String word : words) {
+            if(word.startsWith(pref)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
