b5a23f0970d085ecd54481e262cb160749371860
monoskribt <kozub.dima3@gmail.com>
2025-02-28 21:53:06 +0100

    added new tasks

---
+++ src/Easy/TheLatestLoginIn2020.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class TheLatestLoginIn2020 {
+
+    //# Write your MySQL query statement below
+    //SELECT DISTINCT(user_id), max(time_stamp) as last_stamp
+    //FROM Logins
+    //WHERE YEAR(time_stamp) = 2020
+    //GROUP BY user_id;
+}
---
+++ src/Medium/ExchangeSeats.java
@@ -0,0 +1,16 @@
+package Medium;
+
+public class ExchangeSeats {
+
+//    SELECT
+//            CASE
+//    WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1
+//    THEN id
+//    WHEN id % 2 = 1
+//    THEN id + 1
+//    ELSE
+//    id - 1
+//    END as id, student
+//    FROM Seat
+//    ORDER BY id;
+}
