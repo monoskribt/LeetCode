eb7b06297d43b42fbff4c76a45eade9398c2a44f
monoskribt <kozub.dima3@gmail.com>
2025-03-19 00:09:49 +0100

    LargestNumberAfterDigitSwapsByParity

---
+++ src/Easy/LargestNumberAfterDigitSwapsByParity.java
@@ -0,0 +1,44 @@
+package Easy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LargestNumberAfterDigitSwapsByParity {
+
+    public static int largestInteger(int num) {
+        PriorityQueue<Integer> evenNumbers = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> oddNumbers = new PriorityQueue<>(Comparator.reverseOrder());
+        LinkedList<Integer> originalNums = new LinkedList<>();
+
+        while (num > 0) {
+            int temp = num % 10;
+            num =  num / 10;
+            originalNums.addFirst(temp);
+
+            if(temp % 2 == 0) {
+                evenNumbers.add(temp);
+            }
+            else {
+                oddNumbers.add(temp);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int el : originalNums) {
+            if (el % 2 == 0) {
+                sb.append(evenNumbers.poll());
+            }
+            else {
+                sb.append(oddNumbers.poll());
+            }
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        int result = largestInteger(65875);
+        System.out.println(result);
+    }
+}
